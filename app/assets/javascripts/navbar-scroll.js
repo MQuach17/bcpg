d0237692cc9547b63c6b56afe1bbe2a5ef36a629
Michael Quach <michaelquach@openarc.net>
2016-11-02 00:38:33 -0400

    home styling

---
+++ app/assets/javascripts/navbar-scroll.js
@@ -0,0 +1,14 @@
+(function($){
+	$(document).scroll(function(){
+		offset = $(window).scrollTop();
+		console.log(offset);
+		if(offset > 500){
+			$('.content').addClass('clear-navbar');
+		}
+		else if(offset <= 500){
+			$('.content').removeClass('clear-navbar');
+		}
+	});
+})(jQuery)
+
+
