34ae6776254502e819a24c6f38cb86b89c72cab8
Michael Quach <michaelquach@openarc.net>
2016-11-03 18:47:47 -0400

    views, styles

--- app/assets/javascripts/navbar-scroll.js
+++ app/assets/javascripts/navbar-scroll.js
@@ -4,9 +4,11 @@
 		console.log(offset);
 		if(offset > 500){
 			// $('.content').addClass('clear-navbar');
+			$('.hero-overlay').fadeOut('fast');
 		}
 		else if(offset <= 500){
 			// $('.content').removeClass('clear-navbar');
+			$('.hero-overlay').fadeIn('slow');
 		}
 	});
 })(jQuery)
