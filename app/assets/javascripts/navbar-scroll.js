08aacefe119ec5c8b96fac464fc2ea758544873c
Michael Quach <michaelquach@openarc.net>
2016-11-16 13:37:57 -0500

    layouts

--- app/assets/javascripts/navbar-scroll.js
+++ app/assets/javascripts/navbar-scroll.js
@@ -3,11 +3,9 @@
 		offset = $(window).scrollTop();
 		console.log(offset);
 		if(offset > 500){
-			// $('.content').addClass('clear-navbar');
-			$('.hero-overlay').fadeOut('fast');
+			$('.hero-overlay').hide();
 		}
 		else if(offset <= 500){
-			// $('.content').removeClass('clear-navbar');
 			$('.hero-overlay').fadeIn('slow');
 		}
 	});
